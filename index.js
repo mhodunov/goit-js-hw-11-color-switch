89a92e9ef831392f42ac2fef0115a08a4adf8ad7
Maksym Hodunov <m.godunov@yandex.ru>
2020-12-16 21:37:33 +0200

    adds final solution

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Вводные данные: массив с цветами и рандомайзер
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -11,20 +12,44 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+//Находим в DOM кнопки Start/Stop и body
 const startButton = document.querySelector('[data-action="start"]');
 const stopButton = document.querySelector('[data-action="stop"]');
 const body = document.querySelector('body');
 
-startButton.addEventListener("click", changeColors);
+//ID интервала
+let intervalId = null;
 
-function changeColors() {
+//Вешаем слушатель событий на кнопку Start
+startButton.addEventListener("click", startChangeColors);
+
+//Функция, которая запускается при клике на кнопку Start
+function startChangeColors() {
+  changeColors();
+  intervalId = setInterval(changeColors, 1000);
   
-  body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
-  startButton.removeEventListener("click", changeColors);
+  /*При клике на кнопку Start, с нее удаляется слушатель событий и нажатия на кнопку больше не вызывают функцию, 
+  также вешаем слушатель событий на кнопку Stop*/
+  startButton.removeEventListener("click", startChangeColors);
   stopButton.addEventListener("click", stopChangeColors);
+  
+  /*Альтернативный вариант, при котором кнопка после клика становится неактивной
+  startButton.disabled = "true"; 
+  stopButton.disabled = "false";*/
 }
 
+//Функция, которая вызывается при клике на Stop. Очищается интервал и снова добавляется слушатель событий на Start. С кнопки Stop слушатель удаляется.
 function stopChangeColors() {
+  clearInterval(intervalId);
+  startButton.addEventListener("click", startChangeColors);
+  stopButton.removeEventListener("click", stopChangeColors);
   
-  startButton.addEventListener("click", changeColors);
+  /*Альтернативный вариант с отключением кнопок
+  startButton.disabled = "false";
+  stopButton.disabled = "true"; */
+}
+
+//Функция, которая рандомно задает body один из 6 цветов
+function changeColors() {
+  body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
 }
